ebb4e8a78dc59ec6deecc9491ff53e1b3aef7da1
Teddy <tgamiette@kernix.com>
2024-10-28 14:37:52 +0100

    Add Vector3

--- main.cpp
+++ main.cpp
@@ -8,46 +8,9 @@ using namespace std;
 
 
 int main() {
-    UserInputs userInputs;
+    auto const vector1 = Vector3(0, 0, 0);
+    auto const vector2 = Vector3(255, 255, 2555);
 
-
-    // Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
-
-
-    Color red(1, 0, 0);
-    Color black;
-
-
-    cout << "Red : " << red << std::endl;
-    cout << "Black : " << black << std::endl;
-
-    const int WIDTH = userInputs.AskIntInput("Enter the width of the image : ");
-    const int HEIGHT = userInputs.AskIntInput("Enter the height of the image : ");
-
-
-    // Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(WIDTH) / HEIGHT);
-    auto camera_center = Vector(0, 0, 0);
-
-    Image image(WIDTH, HEIGHT, black);
-
-    int centerX = WIDTH / 2;
-    int centerY = HEIGHT / 2;
-
-
-    int radius = min(WIDTH, HEIGHT) / 4;
-    //
-
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            Ray r(camera_center, ray_direction);
-            Color pixel_color = ray_color(r);
-        }
-    }
-
-    image.WriteFile("../../../circle.png");
+    std::cout << "Vector1 ->" << vector1 << std::endl;
+    std::cout << "Vector2->" << vector2 << std::endl;
 }
