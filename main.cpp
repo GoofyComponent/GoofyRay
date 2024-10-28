9cffdf038b5d634d2ae2a9c78e1b3dfcd43236f7
teddy Kernix <93723692+tgamiette@users.noreply.github.com>
2024-10-28 13:58:13 +0100

    Revert "init object"

--- main.cpp
+++ main.cpp
@@ -1,53 +1,37 @@
 #include <iostream>
 #include "Color.hpp"
 #include "Image.hpp"
-#include "Ray.h"
 #include "Uinputs.hpp"
 
 using namespace std;
 
 
-int main() {
+int main()
+{    
     UserInputs userInputs;
-
-
-    // Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
-
-
+    
     Color red(1, 0, 0);
     Color black;
-
 
     cout << "Red : " << red << std::endl;
     cout << "Black : " << black << std::endl;
 
-    const int WIDTH = userInputs.AskIntInput("Enter the width of the image : ");
-    const int HEIGHT = userInputs.AskIntInput("Enter the height of the image : ");
-
-
-    // Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(WIDTH) / HEIGHT);
-    auto camera_center = Vector(0, 0, 0);
+    int WIDTH = userInputs.AskIntInput("Enter the width of the image : ");
+    int HEIGHT = userInputs.AskIntInput("Enter the height of the image : ");
 
     Image image(WIDTH, HEIGHT, black);
 
     int centerX = WIDTH / 2;
     int centerY = HEIGHT / 2;
-
-
     int radius = min(WIDTH, HEIGHT) / 4;
-    //
 
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            Ray r(camera_center, ray_direction);
-            Color pixel_color = ray_color(r);
+            if ((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY) < radius * radius) {
+                image.SetPixel(x, y, red);
+            }
         }
     }
-
+    
     image.WriteFile("../../../circle.png");
 }
